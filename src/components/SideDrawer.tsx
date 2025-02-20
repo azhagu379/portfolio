1eb6b38d2f0cefcd6fd835d0c23e2191927afd26
azhagu379 <a.palanisamy@memob.com>
2025-02-20 19:30:54 +0530

    update Footer component with author name and add StyledBox component; enhance SideDrawer with new icons and improved hover effects

--- src/components/SideDrawer.tsx
+++ src/components/SideDrawer.tsx
@@ -1,83 +1,79 @@
-import React from 'react';
-import { styled, Theme, useTheme } from '@mui/material/styles';
-import Link from 'next/link';
-import MuiDrawer from '@mui/material/Drawer';
-import Divider from '@mui/material/Divider';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import IconButton from '@mui/material/IconButton';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import HomeIcon from '@mui/icons-material/Home';
-import InfoIcon from '@mui/icons-material/Info';
-import BuildIcon from '@mui/icons-material/Build';
-import ContactsIcon from '@mui/icons-material/Contacts';
-import SchoolIcon from '@mui/icons-material/School';
-import { CSSObject } from '@emotion/react';
+import React from "react";
+import { styled, Theme, useTheme } from "@mui/material/styles";
+import Link from "next/link";
+import MuiDrawer from "@mui/material/Drawer";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import IconButton from "@mui/material/IconButton";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { Icon } from "@iconify/react";
+import { CSSObject } from "@emotion/react";
 
 const drawerWidth = 240;
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
   padding: theme.spacing(0, 1),
   ...theme.mixins.toolbar,
-  position: 'relative',
+  position: "relative",
 }));
 
 const StyledIconButton = styled(IconButton)(({ theme }) => ({
-  position: 'absolute',
-  top: '50%',
-  right: '-15px',
-  transform: 'translateY(-50%)',
-  borderRadius: '50%',
+  position: "absolute",
+  top: "50%",
+  right: "-15px",
+  transform: "translateY(-50%)",
+  borderRadius: "50%",
   boxShadow: theme.shadows[3],
-  '&:hover': {
+  "&:hover": {
     backgroundColor: theme.palette.action.hover,
   },
 }));
 
 const openedMixin = (theme: Theme): CSSObject => ({
   width: drawerWidth - 20,
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
   // border: `2px solid ${theme.palette.secondary.main}`, // Yellow outline for drawer
 });
 
 const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
   width: `calc(${theme.spacing(7)} + 1px)`,
   // border: `2px solid ${theme.palette.secondary.main}`, // Yellow outline for drawer
-  [theme.breakpoints.up('sm')]: {
+  [theme.breakpoints.up("sm")]: {
     width: `calc(${theme.spacing(8)} + 1px)`,
   },
 });
 
 const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
   width: drawerWidth,
   flexShrink: 0,
-  whiteSpace: 'nowrap',
-  boxSizing: 'border-box',
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
   ...(open && {
     ...openedMixin(theme),
-    '& .MuiDrawer-paper': openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
   }),
   ...(!open && {
     ...closedMixin(theme),
-    '& .MuiDrawer-paper': closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
   }),
 }));
 
@@ -88,12 +84,59 @@ interface SideDrawerProps {
 
 const SideDrawer: React.FC<SideDrawerProps> = ({ open, handleDrawerClose }) => {
   const theme = useTheme();
+
+  const iconSize = 28; // Adjust size as needed
+
   const menuItems = [
-    { text: 'Home', icon: <HomeIcon sx={{ color: theme.palette.primary.main }} />, link: '/' },
-    { text: 'About', icon: <InfoIcon sx={{ color: theme.palette.primary.main }} />, link: '/about' },
-    { text: 'Skills', icon: <BuildIcon sx={{ color: theme.palette.primary.main }} />, link: '/skills' },
-    { text: 'Contact', icon: <ContactsIcon sx={{ color: theme.palette.primary.main }} />, link: '/contact' },
-    { text: 'Tutorial', icon: <SchoolIcon sx={{ color: theme.palette.primary.main }} />, link: '/tutorial' },
+    {
+      text: "Home",
+      icon: <Icon icon="ic:round-home" width={iconSize} height={iconSize} />,
+      link: "/",
+    },
+    {
+      text: "About",
+      icon: (
+        <Icon
+          icon="tabler:user-code"
+          width={iconSize}
+          height={iconSize}
+        />
+      ),
+      link: "/about",
+    },
+    {
+      text: "Skills",
+      icon: <Icon icon="hugeicons:knowledge-02" width={iconSize} height={iconSize} />,
+      link: "/skills",
+    },
+    {
+      text: "Contact",
+      icon: (
+        <Icon icon="fluent:mail-24-regular" width={iconSize} height={iconSize} />
+      ),
+      link: "/contact",
+    },
+  ];
+  
+  const bottomItems = [
+    {
+      text: "Tutorial",
+      icon: (
+        <Icon
+         icon="hugeicons:laptop-programming"
+          width={iconSize}
+          height={iconSize}
+        />
+      ),
+      link: "/tutorial",
+    },
+    {
+      text: "Blog",
+      icon: (
+        <Icon icon="material-symbols:menu-book-outline" width={iconSize} height={iconSize} />
+      ),
+      link: "/blog",
+    },
   ];
 
   return (
@@ -110,31 +153,73 @@ const SideDrawer: React.FC<SideDrawerProps> = ({ open, handleDrawerClose }) => {
       <Divider />
       <List>
         {menuItems.map((item) => (
-          <ListItem key={item.text} disablePadding sx={{ display: 'block' }}>
+          <ListItem key={item.text} disablePadding sx={{ display: "block" }}>
             <ListItemButton
               component={Link}
               href={item.link}
               sx={{
                 minHeight: 48,
-                justifyContent: open ? 'initial' : 'center',
+                justifyContent: open ? "initial" : "center",
                 px: 2.5,
-              }}
-            >
+                "&:hover .MuiListItemIcon-root": {
+                  color: theme.palette.secondary.main, // Change color on hover
+                },
+                transition: "all 0.3s ease", // Smooth hover effect
+              }}>
               <ListItemIcon
+                className="MuiListItemIcon-root"
                 sx={{
                   minWidth: 0,
-                  mr: open ? 3 : 'auto',
-                  justifyContent: 'center',
-                }}
-              >
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                  color: theme.palette.primary.main, // Default color
+                  transition: "color 0.3s ease", // Smooth color transition
+                }}>
                 {item.icon}
               </ListItemIcon>
-              <ListItemText primary={item.text} sx={{ opacity: open ? 1 : 0 }} />
+              <ListItemText
+                primary={item.text}
+                sx={{ opacity: open ? 1 : 0 }}
+              />
             </ListItemButton>
           </ListItem>
         ))}
       </List>
       <Divider />
+      <List sx={{ marginTop: "auto" }}>
+        {bottomItems.map((item) => (
+          <ListItem key={item.text} disablePadding sx={{ display: "block" }}>
+            <ListItemButton
+              component={Link}
+              href={item.link}
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+                "&:hover .MuiListItemIcon-root": {
+                  color: theme.palette.secondary.main, // Change color on hover
+                },
+                transition: "all 0.3s ease", // Smooth hover effect
+              }}>
+              <ListItemIcon
+                className="MuiListItemIcon-root"
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                  color: theme.palette.primary.main, // Default color
+                  transition: "color 0.3s ease", // Smooth color transition
+                }}>
+                {item.icon}
+              </ListItemIcon>
+              <ListItemText
+                primary={item.text}
+                sx={{ opacity: open ? 1 : 0 }}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </Drawer>
   );
 };
---
+++ src/components/style/StyledBox.tsx
@@ -0,0 +1,18 @@
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+
+export const StyledProjectBox = styled(Box)(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+  padding: "20px",
+  borderRadius: "12px",
+  textAlign: "center",
+  border: `2px solid ${theme.palette.primary.main}`,
+  transition: "border-color 0.4s ease, box-shadow 0.4s ease, transform 0.4s ease",
+  position: "relative",
+  overflow: "hidden",
+  "&:hover": {
+    borderColor: theme.palette.secondary.main,
+    transform: "rotate(2deg) scale(1.07)",
+    boxShadow: `0px 4px 15px ${theme.palette.secondary.main}, 0px 0px 10px ${theme.palette.primary.main}`,
+  },
+}));
